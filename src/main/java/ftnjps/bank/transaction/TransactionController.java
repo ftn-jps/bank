fc9103b23f6c10bc18b85190eef93eec7a518b1d
Ivan Jancic <ivan248959@gmail.com>
2018-11-29 17:55:53 +0100

    Changes reverted.

--- src/main/java/ftnjps/bank/transaction/TransactionController.java
+++ src/main/java/ftnjps/bank/transaction/TransactionController.java
@@ -1,27 +1,20 @@
-package ftnjps.paymentgateway.transaction;
-
-import java.net.URI;
+package ftnjps.bank.transaction;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import ftnjps.paymentgateway.merchant.MerchantService;
+import ftnjps.bank.carddetails.CardDetails;
+import ftnjps.bank.process.ProcessPayment;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -30,12 +23,12 @@ public class TransactionController {
 	@Autowired
 	private TransactionService transactionService;
 	@Autowired
-	private MerchantService merchantService;
-	@Autowired
-	RestTemplate restClient;
+	private ProcessPayment processPayment;
 
-	@Value("${frontend.port}")
+	@Value("${server.port}")
 	private int port;
+	@Value("${bank.iin}")
+	private String bankIin;
 
 	@PostMapping
 	public ResponseEntity<?> startTransaction(@RequestBody @Valid Transaction transaction) {
@@ -47,56 +40,31 @@ public class TransactionController {
 		return new ResponseEntity<>(headers, HttpStatus.FOUND);
 	}
 
-	@GetMapping("/{token}/type/{paymentType}")
-	public ResponseEntity<?> forwardTransaction(
+	@PostMapping("/{token}")
+	public ResponseEntity<?> processTransaction(
 			@PathVariable String token,
-			@PathVariable PaymentType paymentType) {
+			@RequestBody @Valid CardDetails cardDetails) {
 		Transaction transaction = transactionService.findByToken(token);
 
-		if(paymentType == PaymentType.BITCOIN) {
-			
-			String url = "https://api-sandbox.coingate.com/v2/orders";
-			
+		if(cardDetails.getPan().startsWith(bankIin)) {
 			HttpHeaders headers = new HttpHeaders();
-			headers.set("Content-Type", "application/x-www-form-urlencoded");
-			headers.set("Authorization", "Token FzQjbFWsjfH4LtVzwse6c33hGBWa1fiYag8g24ou");
-			
-			MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
-			map.add("order_id", transaction.getId() + "");
-			map.add("price_amount", transaction.getAmount() + "");
-			map.add("price_currency", "USD");
-			map.add("receive_currency", "USD");
-			map.add("title", token);
-			HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
-			
-			ResponseEntity<String> response = restClient.postForEntity(url, request, String.class);
-			
-			String[] split1 = response.getBody().split(":"); // gives array of 16 strings made from JSON object
-			String[] split2 = split1[15].split("\",");      // takes **** //sandbox.coingate.com/invoice/e4ba2d6b-a0be-43bc-943c-c76233c18b19","token" ****
-			String paymentUrl = "https:" + split2[0];      // and converts it into array of 2 strings where split2[0] is the url
 
-			HttpHeaders headersForRedirectingToBitcoinPaymentURL = new HttpHeaders();
-			headersForRedirectingToBitcoinPaymentURL.add("Location", paymentUrl);
-			
-			return new ResponseEntity<>(headersForRedirectingToBitcoinPaymentURL, HttpStatus.FOUND);
-			
+			if(processPayment.local(cardDetails, transaction))
+				headers.add("Location",
+						transaction.getSuccessUrl());
+			else
+				headers.add("Location",
+						transaction.getFailUrl());
+
+			return new ResponseEntity<>(headers, HttpStatus.SEE_OTHER);
 		}
-		
-		if(paymentType != PaymentType.BANK) // TODO
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-
-		// BANK
-		String bankUrl = merchantService
-				.findByMerchantId(transaction.getMerchantId())
-				.getBankUrl();
-		URI response = restClient.postForLocation(
-				bankUrl + "/api/transactions",
-				transaction);
-		String paymentUrl = response.toString();
+		else {
+			// TODO Forward to PCC
+		}
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Location",
-				paymentUrl);
-		return new ResponseEntity<>(headers, HttpStatus.FOUND);
+				transaction.getErrorUrl());
+		return new ResponseEntity<>(headers, HttpStatus.SEE_OTHER);
 	}
 }
